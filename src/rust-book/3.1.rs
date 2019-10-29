fa8861574a2be330f5418c4373ee635cd14b8401
alienzj <alienchuj@gmail.com>
2019-10-29 13:39:49 +0800

    update

--- src/rust-book/3.1.rs
+++ src/rust-book/3.1.rs
@@ -1,7 +1,21 @@
 fn main()
 {
-    let x = 5;
+    let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    const MAX_POINTS: u32 = 100_000;
+    println!("The const MAX_POINTS is: {}", MAX_POINTS);
+
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("The value of y is: {}", y);
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+    // let mut spaces = "   ";
+    // spaces = spaces.len();
+    println!("The value of spaces is: {}", spaces);
 }
