0a03b75108b5f892678727f85518c28dfb7c342f
alienzj <alienchuj@gmail.com>
2020-01-23 18:37:41 +0800

    update

--- src/rust-book/4.3.rs
+++ src/rust-book/4.3.rs
@@ -7,6 +7,8 @@ fn main() {
 fn first_word(s: &String) -> usize {
     let bytes = s.as_bytes(); // convert String to an array of bytes
 
+    // create iterator over the array of bytes using the iter method
+    // (index of the element, reference to the element)
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
             return i;
