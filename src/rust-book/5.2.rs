c61ba4e797e7a2f6fca5f81e097825a591622650
alienzj <alienchuj@gmail.com>
2020-02-01 15:11:11 +0800

    add 5.2.rs

---
+++ src/rust-book/5.2.rs
@@ -0,0 +1,43 @@
+fn main() {
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+
+    let rect1 = (30, 50);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_v2(rect1)
+    );
+
+    let rect2 = Rectangle { width: 30, height: 50 };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_v3(&rect2)
+    );
+
+    // println!("rect2 is {:#?}", rect2);
+    println!("rect2 is {:?}", rect2);
+    println!("rect2 is {:#?}", rect2);
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area_v2(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area_v3(rect: &Rectangle) -> u32 {
+    rect.width * rect.height
+}
