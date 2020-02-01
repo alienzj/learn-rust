ea5957dd53e82f589cfe376c407fa901914803ec
alienzj <alienchuj@gmail.com>
2020-02-02 00:38:41 +0800

    add 6.1.rs

---
+++ src/rust-book/6.1.rs
@@ -0,0 +1,71 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr_ {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn route(ip_kind: IpAddrKind) {
+   
+}
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+//#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// struct QuitMessage;
+// struct MoveMessage {
+//     x: i32,
+//     y: i32,
+// }
+// struct WriteMessage(String);
+// struct ChangeColorMessage(i32, i32, i32);
+
+impl Message {
+    fn call(&self) {
+        //
+    }
+}
+
+fn main() {
+    let _four = IpAddrKind::V4;
+    let _six = IpAddrKind::V6;
+
+    let _home_ = IpAddr_ {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let _loopback_ = IpAddr_ {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    let _home = IpAddr::V4(127, 0, 0, 1);
+    let _loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    // let sum = x + y;
+}
