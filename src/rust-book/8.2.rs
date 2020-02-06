dc6a08631fac1346eff52f666311b26311dc549c
alienzj <alienchuj@gmail.com>
2020-02-06 21:06:48 +0800

    update

--- src/rust-book/8.2.rs
+++ src/rust-book/8.2.rs
@@ -20,13 +20,52 @@ fn main() {
     let _hello = String::from("Здравствуйте");
     let _hello = String::from("Hola");
 
-    let _hello_world = String::from("hello") + &String::from(" world");
+    let hello = String::from("Hello, ");
+    let world = String::from("world!");
+    // let _helo_world = hello + &world;
+    // // note hello has been moved here and can no longer be used
+
+    // deref coercion
+    // &String -> &str
+    // &world -> &world[..]
+    let _helo_world = hello + &world[..];
+    // println!("{}", hello);
+    println!("{}", world);
+
 
     let mut foobar = String::from("foo");
     foobar.push_str("bar");
 
-    let mut s1 = String::from("foo");
-    let s2 = "bar";
-    s1.push_str(s2);
-    println!("s2 is {}", s2);
+    let mut v1 = String::from("foo");
+    let v2 = "bar";
+    v1.push_str(v2);
+    println!("v2 is {}", v2);
+
+    let mut v3 = String::from("lo");
+    v3.push('l');
+    // v3.push_str("l");
+    println!("v3 is {}", v3);
+
+    let ss1 = String::from("tic");
+    let ss2 = String::from("tac");
+    let ss3 = String::from("toe");
+
+    // let ss = ss1 + "-" + &ss2 + "-" + &ss3;
+    let _sss = format!("{}-{}-{}", ss1, ss2, ss3);
+    // let h = _sss[0];
+
+    let _len = String::from("Здравствуйте").len();
+    let _xxx = "Здравствуйте";
+    // let answer = &_xxx[0];
+    let answer = &_xxx[0..4];
+    println!("answer is {}", answer);
+
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+
+    for b in "नमस्ते".bytes() {
+        println!("{}", b);
+    }
+   
 }
