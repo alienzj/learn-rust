00d83da6a00d6d0571f985299654624915b0fb8e
alienzj <alienchuj@gmail.com>
2020-02-07 14:00:47 +0800

    update

--- src/rust-book/10.1.rs
+++ src/rust-book/10.1.rs
@@ -24,23 +24,97 @@ fn main() {
     // println!("The largest number is {}", largest);
 
     let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
     println!("The largest number is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
     println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
+
+    let integer = Point { x: 5, y: 10 };
+    println!("integer.x = {}", integer.x());
+
+    let float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point2 { x: 5, y: 4.0 };
+
+    let p1 = Point2 { x: 5, y: 10.4 };
+    let p2 = Point2 { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}. p3.y = {}", p3.x, p3.y);
 
 }
 
-fn largest(list: &[i32]) -> i32 {
+fn largest_i32(list: &[i32]) -> i32 {
     let mut largest = list[0];
 
-    for &item in list {
+    for &item in list.iter() {
         if item > largest {
             largest = item;
         }
     }
 
     largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+// the type Point<f32> will have a method named distance_from_origin
+// and other instances of Point<T> where T is not of type f32 will not have this method defined
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+// some generic parameters are declared with impl
+// some generic parameters are declared with the method definition
+impl<T, U> Point2<T, U> {
+    fn mixup<V, W>(self, other: Point2<V, W>) -> Point2<T, W> {
+        Point2 {
+            x: self.x,
+            y: other.y,
+        }
+    }
 }
