52682b9ae2582c69d06295ce30949104cd39ee08
alienzj <alienchuj@gmail.com>
2020-02-11 00:36:39 +0800

    update

--- src/rust-book/10.1.rs
+++ src/rust-book/10.1.rs
@@ -24,15 +24,18 @@ fn main() {
     // println!("The largest number is {}", largest);
 
     let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&number_list);
+    // let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest_i32(&number_list);
+    // let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&char_list);
+    // let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 
     let integer = Point { x: 5, y: 10 };
@@ -45,34 +48,9 @@ fn main() {
     let p2 = Point2 { x: "Hello", y: 'c' };
     let p3 = p1.mixup(p2);
     println!("p3.x = {}. p3.y = {}", p3.x, p3.y);
-
 }
 
-fn largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-// fn largest<T>(list: &[T]) -> T {
+// fn largest_i32(list: &[i32]) -> i32 {
 //     let mut largest = list[0];
 
 //     for &item in list.iter() {
@@ -83,6 +61,30 @@ fn largest_char(list: &[char]) -> char {
 
 //     largest
 // }
+
+// fn largest_char(list: &[char]) -> char {
+//     let mut largest = list[0];
+
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
 
 struct Point<T> {
     x: T,
--- src/rust-book/10.2.rs
+++ src/rust-book/10.2.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 pub trait Summary {
     fn summarize(&self) -> String {
         String::from("(Read more...)")
@@ -53,7 +55,45 @@ pub fn notify_full<T: Summary>(item: T) {
 // pub fn notify(item: impl Summary + Display)
 // pub fn notify<T: Summary + Display>(item: T)
 
+// fn some_function<T: Display + Clone, U: Clone + Debug>(t: T, u: U) -> i32 {}
+// fn some_function<T, U>(t: T, u: U) -> i32
+//     where T: Display + Clone,
+//           U: Clone + Debug {}
 
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self {
+            x,
+            y,
+        }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
+
+// impl<T: Display> ToString for T {}
 
 fn main() {
     let tweet = Tweet {
