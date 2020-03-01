157325f153a56357ecc42f0c38234526aac229f4
alienzj <alienchuj@gmail.com>
2020-03-02 05:23:44 +0800

    update

--- src/rust-book/10.3.rs
+++ src/rust-book/10.3.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn main() {
     let r;
     //{
@@ -26,6 +28,16 @@ fn main() {
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn lonagest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where T: Display {
+    println!("Announcement! {}", ann);
     if x.len() > y.len() {
         x
     } else {
