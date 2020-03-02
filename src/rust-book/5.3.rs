ee8448a299bf4ccb90aedc5e6c9cf0eccde70a41
alienzj <alienchuj@gmail.com>
2020-03-03 02:53:06 +0800

    update

--- src/rust-book/5.3.rs
+++ src/rust-book/5.3.rs
@@ -46,3 +46,14 @@ fn main() {
     let sq = Rectangle::square(3);
     println!("squair is {:#?}", sq);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle { width: 8, height: 7 };
+        let smaller = Rectangle { width: 5, height: 1 };
+        assert!{larger.can_hold(&smaller)};
+    }
+}
--- src/rust-book/adder/src/lib.rs
+++ src/rust-book/adder/src/lib.rs
@@ -1,7 +1,58 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub fn greeting_hello(name: &str) -> String {
+    String::from("Hello!")
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    fn exploration() {
+        assert_eq!("hello world", "hello".to_owned() + " world");
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+
+    #[test]
+    fn add_two_test() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting dit not contain name, values was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    fn greeting_no_name() {
+        let result = greeting_hello("hello");
+        assert!(result.contains("Hello"));
+    }
+}
+
+//#[cfg(test)]
+#[test]
+fn hello_test() {
+    assert_ne!(1, 2);
 }
