0c4b47d67b873a1a40d81f142bc726cb05ec18ea
alienzj <alienchuj@gmail.com>
2020-03-03 23:29:01 +0800

    add 13.1.rs

---
+++ src/rust-book/13.1.rs
@@ -0,0 +1,18 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user
+    )
+
+}
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
