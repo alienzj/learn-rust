d67e7c355e4ae4377050ed9f8afc952f5bda4290
alienzj <alienchuj@gmail.com>
2020-03-03 18:19:32 +0800

    update

--- src/rust-book/adder/src/lib.rs
+++ src/rust-book/adder/src/lib.rs
@@ -10,6 +10,22 @@ pub fn greeting_hello(name: &str) -> String {
     String::from("Hello!")
 }
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
