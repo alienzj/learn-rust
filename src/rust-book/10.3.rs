94f60e1400907fd948e7521eedc2af02c3b5c83d
alienzj <alienchuj@gmail.com>
2020-03-03 15:57:34 +0800

    update

--- src/rust-book/10.3.rs
+++ src/rust-book/10.3.rs
@@ -68,6 +68,31 @@ fn main_test() {
 //fn longest<'a>(x: &'a str, y: &str) -> &'a str {}
 //fn longest<'a>(x: &str, y: &str) -> &'a str {}
 
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+// fn first_word<'a>(s: &'a str) -> &'a str {}
+
 struct ImportantExcerpt<'a> {
     part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
 }
