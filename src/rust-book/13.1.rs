810c3a3b02153889859b7af9d8ee511cbe601a57
alienzj <alienchuj@gmail.com>
2020-03-07 20:18:30 +0800

    Fn, FnMut, FnOnce

--- src/rust-book/13.1.rs
+++ src/rust-book/13.1.rs
@@ -48,7 +48,10 @@ fn generate_workout(intensity: u32, random_number: u32) {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes!", expensive_result.value(intensity));
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
         }
     }
 }
@@ -77,12 +80,13 @@ where
 }
 
 impl<T> Cacher<T>
-where T: Fn(u32) -> u32
+where
+    T: Fn(u32) -> u32,
 {
     fn new(calculation: T) -> Cacher<T> {
         Cacher {
             calculation,
-            value: None
+            value: None,
         }
     }
 
@@ -93,7 +97,53 @@ where T: Fn(u32) -> u32
                 let v = (self.calculation)(arg);
                 self.value = Some(v);
                 v
-            },
+            }
         }
     }
+}
+
+#[test]
+fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(v2, 2);
+}
+
+#[test]
+fn capturing_env_with_closures() {
+    let x = 4;
+
+    let equal_to_x = |z| z == x;
+
+    let y = 4;
+
+    assert!(equal_to_x(y));
+}
+
+#[test]
+fn capturing_env_with_fn() {
+    let x = 4;
+
+    fn equal_to_x(z: i32) -> bool {
+        z == x
+    }
+
+    let y = 4;
+
+    assert!(equal_to_x(y));
+}
+
+#[test]
+fn capturing_env_with_closures_move() {
+    let x = vec![1, 2, 3];
+
+    let equal_to_x = move |z| z == x;
+
+    println!("can't use x here: {:?}", x);
+
+    let y = vec![1, 2, 3];
+
+    assert!(equal_to_x(y));
 }
