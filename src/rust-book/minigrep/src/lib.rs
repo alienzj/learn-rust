4e7cf9d2019cbf6a848ef41e450239ce6cb48c7a
alienzj <alienchuj@gmail.com>
2020-03-07 13:08:57 +0800

    test sucess

--- src/rust-book/minigrep/src/lib.rs
+++ src/rust-book/minigrep/src/lib.rs
@@ -25,7 +25,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -41,7 +49,7 @@ safe, fast, productive.
 Pick three.";
 
         assert_eq!(
-            vec!["save, fast, productive."],
+            vec!["safe, fast, productive."],
             search(query, contents)
         );
     }
