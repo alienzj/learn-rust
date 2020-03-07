4be24efb87094cf2a496bcafa03d0e62fd6c5c7a
alienzj <alienchuj@gmail.com>
2020-03-07 10:05:30 +0800

    use pattern match

--- src/rust-book/minigrep/src/main.rs
+++ src/rust-book/minigrep/src/main.rs
@@ -4,15 +4,15 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    if let Ok(config) = Config::new(&args) {
+        println!("Searching for {}", config.query);
+        println!("In file {}", config.filename);
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+        let contents =
+            fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+        println!("With text:\n{}", contents);
+    }
 }
 
 struct Config {
