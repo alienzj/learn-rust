7148eb4647c912e951540af4d66189c0a32d6e84
alienzj <alienchuj@gmail.com>
2020-03-07 19:48:42 +0800

    save

--- src/rust-book/13.1.rs
+++ src/rust-book/13.1.rs
@@ -5,39 +5,43 @@ fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
 
-    generate_workout(
-        simulated_user_specified_value,
-        simulated_random_number
-    );
+    fn  add_one_v1   (x: u32) -> u32 { x + 1 }
+    let add_one_v2 = |x: u32| -> u32 { x + 1 };
+    //let add_one_v3 = |x: u32|        { x + 1 };
+    //let add_one_v4 = |x|               x + 1  ;
 
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+
+    let example_closure = |x| x;
+    let s = example_closure(String::from("hello"));
+    // type locked in the closure
+    // let n = example_closure(5);
 }
 
+/*
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
     intensity
 }
+*/
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = simulated_expensive_calculation(intensity);
+    //let expensive_result = simulated_expensive_calculation(intensity);
+    let expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
 
     if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            expensive_result
-        );
-        println!(
-            "Next, do {} situps!",
-            expensive_result
-        );
+        println!("Today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!(
-                "Today, run for {} minutes!",
-                expensive_result
-            );
+            println!("Today, run for {} minutes!", expensive_closure(intensity));
         }
     }
 }
