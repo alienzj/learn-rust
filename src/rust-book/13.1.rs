87683dcb78f84166e8cb42776cab3f64184c4fe4
alienzj <alienchuj@gmail.com>
2020-03-07 17:51:09 +0800

    add 13.1.rs

--- src/rust-book/13.1.rs
+++ src/rust-book/13.1.rs
@@ -6,8 +6,9 @@ fn main() {
     let simulated_random_number = 7;
 
     generate_workout(
-        simulated_user
-    )
+        simulated_user_specified_value,
+        simulated_random_number
+    );
 
 }
 
@@ -15,4 +16,26 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
     intensity
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            simulated_expensive_calculation(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            simulated_expensive_calculation(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                simulated_expensive_calculation(intensity)
+            );
+        }
+    }
 }
